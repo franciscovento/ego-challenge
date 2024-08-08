8af04dec7371bc0ccfa8acbd67f9d08a99ac3d99
venfran <fgvr92@gmail.com>
2024-08-08 19:13:08 -0300

    feat: add config files and header component

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'app-primary': "#000000",
+        'app-accent': "#EB0A1E",
+        'app-text': "#373737", 
+        'app-bg-primary': "#FFFFFF", 
+        'app-bg-secondary': "#F7F7F7",
+      }
+    },
+  },
+  plugins: [],
+};
