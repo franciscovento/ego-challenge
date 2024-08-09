483b71f615cb5a38ffce661e142af953afcc04ff
venfran <fgvr92@gmail.com>
2024-08-09 20:44:54 -0300

    fix: overflow

--- src/components/ui/client/Menu.tsx
+++ src/components/ui/client/Menu.tsx
@@ -6,9 +6,9 @@ const Menu = () => {
 
   useEffect(() => {
     if (open) {
-      document.body.style.overflow = 'hidden';
+      document.body.style.overflowY = 'hidden';
     } else {
-      document.body.style.overflow = 'auto';
+      document.body.style.overflowY = 'auto';
     }
   }, [open]);
 
